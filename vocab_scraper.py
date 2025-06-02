54dc939026f69353648786d17bf3b59df43e9117
Jan Rau <jan.rau@emmet-software-labs.com>
2025-06-02 10:42:32 +0200

    find translations of one

--- vocab_scraper.py
+++ vocab_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
 
 
 
@@ -9,16 +10,49 @@ def scrape_vocab_once():
         "User-Agent": "Mozilla/5.0"
     }
 
+    id = "115002"
+
     response = requests.get(url, headers=headers)
     
     soup = BeautifulSoup(response.text, "html.parser")
 
-    if response.status_code == 200:
-        jukugo_wrapper = soup.find("div", id="jukugo_115002")
-        print(jukugo_wrapper)
-            
-    else:
-        print("Failed to fetch page:", response.status_code)
+    jukugo_wrapper = soup.find("div", id=f"jukugo_{id}")
+
+
+
+    # SYMBOLS
+    #
+    #
+
+    jap_span = jukugo_wrapper.find("span", id=f"jk_jk_{id}_fc")
+
+    print(jap_span)
+
+    #
+    #
+    #
+
+
+    
+
+    # TRANSLATIONS
+    #
+    #
+
+    vm_divs = jukugo_wrapper.find_all("div", class_="vm")
+
+    print(', '.join(
+        ''.join(child.strip() for child in div.contents if isinstance(child, NavigableString) and child.strip())
+        for div in vm_divs
+    ))
+    
+    #
+    #
+    #
+
+
+
+
         
         
 def scrape_vocab_all_one_link():
